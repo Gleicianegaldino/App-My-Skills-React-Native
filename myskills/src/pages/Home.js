2062fdc0ba97ff11fa4e008f3c1bc76cd2da947a
Gleiciane Galdino <gleicianegaldino25@gmail.com>
2023-03-28 18:36:26 -0300

    Utilizando StyleSheet

--- myskills/src/pages/Home.js
+++ myskills/src/pages/Home.js
@@ -1,16 +1,80 @@
 import React from 'react';
-import { View, Text } from 'react-native';
 
-export function Home(){
+import {  View, 
+          Text, 
+          StyleSheet, 
+          TextInput, 
+          Platform,
+          TouchableOpacity 
+        } from 'react-native';
+
+
+export function Home() {
 
   return(
    
-      <view style={{ flex: 1, justifyContent: 'center',alignItems: 'center' }}>
-        <text>React Native</text>
-        <Text>Ignite</Text>
-      </view>
+      <view style = { styles.container }>
+        <text style = { styles.title }>Welcome, Gleiciane</text>
 
-    
+        <TextInput 
+            style = { styles.input }
+            placeholder = 'New Skill'
+            placeholderTextColor = '555'
+
+        />
+
+        <TouchableOpacity style = { styles.button } activeOpacity={.7}>
+            <text style={ styles.buttonText }>Add</text>
+        </TouchableOpacity>
+
+        <Text style = {[ styles.title, { marginTop: 50 }]}>
+          My Skills
+        </Text>
+
+
+      </view>
     
   )
 }
+
+const styles = StyleSheet.create({
+    
+  container: {
+    flex: 1, 
+    backgroundColor: '#121015',
+    paddingHorizontal: 20,
+    paddingVertical: 70,
+    paddingHorizontal: 30
+
+  },
+  
+  title: {
+    color: '#fff',
+    fontSize: 24,
+    fontWeight: 'bold'
+  },
+
+  input: {
+    backgroundColor: '#1f1e25',
+    color: '#fff',
+    fontSize: 18,
+    padding: Platform.OS === 'ios' ? 15 : 10,
+    marginTop: 30,
+    borderRadius: 7
+  },
+
+  button: {
+    backgroundColor: '#a370f7',
+    padding: 15,
+    borderRadius: 7,
+    alignItems: 'center',
+    marginTop: 20
+  },
+
+  buttonText: {
+    color: '#fff',
+    fontSize: 17,
+    fontWeight: 'bold'
+  }
+
+});
