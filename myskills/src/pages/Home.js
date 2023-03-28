85e6e1f4e14bac1c60a860264987b55e62520236
Gleiciane Galdino <gleicianegaldino25@gmail.com>
2023-03-27 22:09:12 -0300

    Estrutura básica JSX

---
+++ myskills/src/pages/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export function Home(){
+
+  return(
+   
+      <view style={{ flex: 1, justifyContent: 'center',alignItems: 'center' }}>
+        <text>React Native</text>
+        <Text>Ignite</Text>
+      </view>
+
+    
+    
+  )
+}
