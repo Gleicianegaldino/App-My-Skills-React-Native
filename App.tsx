3463d7854cb0f71da6d46796d7d2483558a18104
Gleiciane Galdino <gleicianegaldino25@gmail.com>
2023-03-31 17:55:48 -0300

    StatusBar

--- App.tsx
+++ App.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 
+import { StatusBar } from "react-native";
+
 import { Home } from './src/pages/Home.js';
 
 export default function App(){
 
-  return <Home />
-     
-    
+  return (
   
+    <>
+    <StatusBar barStyle = "light-content" />
+    <Home/>
+    </>
+  )
 }
